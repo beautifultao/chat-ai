2b26ad3d5ed3f03d045cf99d0a7f3e51b882c636
陈涛 <1147589357@qq.com>
2024-09-05 10:51:36 +0800

    Initial commit

---
+++ src/main/java/cn/bugstack/chatgpt/Main.java
@@ -0,0 +1,11 @@
+package cn.bugstack.chatgpt;
+
+/**
+* @DateTime: 2024/9/5
+* @Description: TODO(一句话描述此类的作用)
+* @Author: 阿涛
+**/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
